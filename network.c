d58e235ca727e9a40f425e95094e7c8e8f435fa3
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-04-17 23:53:37 -0700

    lols

--- network.c
+++ network.c
@@ -149,6 +149,7 @@ void net_classify(network_t *net, volume_t **input, double **likelihoods, int n)
       for (int i = 0; i < n; i++) {
           copy_volume(b[0][0], input[i]);
           net_forward(net, b, 0, 0);
+          #pragma omp for
           for (int j = 0; j < NUM_CLASSES; j++) {
               likelihoods[i][j] = b[11][0]->weights[j];
           }
