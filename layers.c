e877d243c41aec4e231c8e1af26004acad5b0301
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-04-17 23:36:15 -0700

    lol

--- layers.c
+++ layers.c
@@ -126,7 +126,7 @@ void conv_forward(conv_layer_t *l, volume_t **inputs, volume_t **outputs, int st
                                 __m256d filter_temp;
                                 __m256d temp;
                                 double A[4];
-                                for(int fd = 0; fd < filter_depth/16 * 16; fd+=16) {
+                                for(int fd = 0; fd < filter_depth/20 * 20; fd+=20) {
                                     filter_temp = _mm256_loadu_pd(filter_weights + ((filter_width * fy) + fx) * filter_depth + fd);
                                     temp = _mm256_loadu_pd(in_weights + ((in_width * in_y) + in_x) * in_depth + fd);
                                     temp = _mm256_mul_pd(filter_temp, temp);
@@ -146,9 +146,14 @@ void conv_forward(conv_layer_t *l, volume_t **inputs, volume_t **outputs, int st
                                     temp = _mm256_loadu_pd(in_weights + ((in_width * in_y) + in_x) * in_depth + fd + 12);
                                     temp = _mm256_mul_pd(filter_temp, temp);
                                     sum = _mm256_add_pd(temp, sum);
+
+                                    filter_temp = _mm256_loadu_pd(filter_weights + ((filter_width * fy) + fx) * filter_depth + fd + 16);
+                                    temp = _mm256_loadu_pd(in_weights + ((in_width * in_y) + in_x) * in_depth + fd + 16);
+                                    temp = _mm256_mul_pd(filter_temp, temp);
+                                    sum = _mm256_add_pd(temp, sum);
                                 }
                                 _mm256_storeu_pd(A, sum);
-                                for(int fd = filter_depth / 16 * 16; fd < filter_depth; fd++) {
+                                for(int fd = filter_depth / 20 * 20; fd < filter_depth; fd++) {
                                     A[0] += filter_weights[((filter_width * fy) + fx) * filter_depth + fd] * in_weights[((in_width * in_y) + in_x) * in_depth + fd];
                                 }
                                 result += A[0];
