1220a9e46d392cd3477f9ef18a0a6c120318093e
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-04-17 21:40:04 -0700

    lol

--- layers.c
+++ layers.c
@@ -171,8 +171,7 @@ void conv_load(conv_layer_t *l, const char *file_name) {
     int filter_width, filter_height, depth, filters;
 
     FILE *fin = fopen(file_name, "r");
-    #pragma omp parallel
-    {
+
     fscanf(fin, "%d %d %d %d", &filter_width, &filter_height, &depth, &filters);
     assert(filter_width == l->filter_width);
     assert(filter_height == l->filter_height);
@@ -180,12 +179,13 @@ void conv_load(conv_layer_t *l, const char *file_name) {
     assert(filters == l->output_depth);
 
     volume_t **l_filters = l->filters;
-    #pragma omp for
+
     for(int f = 0; f < filters; f++) {
       volume_t *filter = l_filters[f];
       double *filter_weights = filter->weights;
       int filter_width = filter->width;
       int filter_depth = filter->depth;
+      #pragma omp parallel for
       for (int x = 0; x < filter_width; x++) {
           for (int y = 0; y < filter_height; y++) {
                 int index = (filter_width * y + x) * filter_depth;
@@ -204,7 +204,7 @@ void conv_load(conv_layer_t *l, const char *file_name) {
         fscanf(fin, "%lf", &val);
         weights[d] = val;
     }
-}
+
     fclose(fin);
 }
 
