c83ac1a0db7323e94e00a9454809381899fbfb1c
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-04-17 21:13:47 -0700

    lol

--- network.c
+++ network.c
@@ -141,15 +141,17 @@ void net_forward(network_t *net, batch_t *b, int start, int end) {
 }
 
 void net_classify(network_t *net, volume_t **input, double **likelihoods, int n) {
-    batch_t *b = make_batch(net, 1);
-
-    for (int i = 0; i < n; i++) {
-        copy_volume(b[0][0], input[i]);
-        net_forward(net, b, 0, 0);
-        for (int j = 0; j < NUM_CLASSES; j++) {
-            likelihoods[i][j] = b[11][0]->weights[j];
-        }
+    #pragma omp parallel
+    {
+      batch_t *b = make_batch(net, 1);
+      for (int i = 0; i < n; i++) {
+          copy_volume(b[0][0], input[i]);
+          net_forward(net, b, 0, 0);
+          for (int j = 0; j < NUM_CLASSES; j++) {
+              likelihoods[i][j] = b[11][0]->weights[j];
+          }
+      }
     }
-
+    
     free_batch(b, 1);
 }
