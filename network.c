bf01ea3de3d6a55bd98545050d79dc3ae1d1e5bd
Tarsus Lam <tarususu.t3@berkeley.edu>
2019-04-17 21:23:19 -0700

    lol

--- network.c
+++ network.c
@@ -104,8 +104,11 @@ void free_network(network_t *net) {
 }
 
 batch_t *make_batch(network_t *net, int size) {
+  #pragma omp parallel
+  {
     batch_t *out = (batch_t*) malloc(sizeof(volume_t **) * (NUM_LAYERS + 1));
     volume_t **layers = net->layers;
+    #pragma omp for
     for (int i = 0; i < NUM_LAYERS + 1; i++) {
         out[i] = (volume_t **) malloc(sizeof(volume_t *)*size);
         volume_t *volume = make_volume(layers[i]->width, layers[i]->height, layers[i]->depth, 0.0);
@@ -114,6 +117,7 @@ batch_t *make_batch(network_t *net, int size) {
         }
     }
     return out;
+  }
 }
 
 void free_batch(batch_t *b, int size) {
